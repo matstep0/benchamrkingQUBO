0905cba74c3286d6854a4669b319a0eec5057acd
matstep0 <mat.stepniak.algo@gmail.com>
2023-03-04 16:40:18 +0100

    Added solution extraction

--- Quantum_Simple_Problem.py
+++ Quantum_Simple_Problem.py
@@ -17,6 +17,7 @@ class  Problem_Generator:
         self.b=None
         self.x=None    
         self.H=0
+        self.gen_sol=None
         
     def __gen_A(self):
         "Generate random instance of traingle matrix with given density"
@@ -32,15 +33,6 @@ class  Problem_Generator:
         #Getting exact solution to minimizing problem
         return np.random.randint(2,size=(self.size,1))
     
-    def get_A(self):
-        return self.A
-    
-    def get_x(self):
-        return self.x
-    
-    def get_b(self):
-        return self.b
-
     def __gen_b(self):
         #Gettin b based on generated A and x b=Ax
         return np.matmul(self.A,self.x)    
@@ -54,9 +46,6 @@ class  Problem_Generator:
     def get_b(self):
         return self.b
     
-    def solution(self):
-        return np.ndarray.flatten(self.x)
-
     def generate_problem(self):
         self.A=self.__gen_A()
         self.x=self.__gen_x()
@@ -88,10 +77,24 @@ class  Problem_Generator:
         sampleset = sampler.sample(bqm, num_reads=10)
         decoded_samples = self.H.decode_sampleset(sampleset)
         best_sample = min(decoded_samples, key=lambda x: x.energy)
-        return best_sample.sample 
-     
+        self.gen_sol=best_sample.sample 
+        return self.gen_sol
+    
+    def solution(self):
+        """Gives generated solution as list"""
+        print(type(self.gen_sol))
+        res=[]
+        for i in range(1,self.size+1):
+            res.append(self.gen_sol['x'+str(i)])
+        return np.array(res) 
+    
+    def real_solution(self):
+        """Give solution as 1-dim numpy array"""
+        return np.ndarray.flatten(self.x)
+    
+ 
 
-       
+"""Execution code for testing"""
 
 x=Problem_Generator(3,0.4)
 x.generate_problem()
@@ -99,8 +102,18 @@ x.Ising_Hamiltonian() ##H wychodzi jako tablica z jednym elementem...
 x.compile()
 qubo, offset = x.to_qubo()
 print(qubo)
-print(x.anneal())
-print(x.solution())
+planted_solution=x.real_solution()
+x.anneal()
+generated_solution=x.solution()
+print(planted_solution)
+print(generated_solution)
+
+
+error_planted=np.sum((np.matmul(x.get_A(),x.get_x()) - x.get_b())**2)    #square error
+error_generated=np.sum((np.matmul(x.get_A(),generated_solution.transpose()) - x.get_b())**2)
+
+print(error_planted)
+print(error_generated)
 
 #qubo, offset = model.to_qubo()
 #print(qubo)
